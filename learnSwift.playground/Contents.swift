57403e6d68fd39d8dd493caf43337ef6abdac114
廖哲澄 <liaozhedeng@liaozhedengdeMacBook-Pro.local>
2021-11-11 20:46:58 +0800

    Basic

---
+++ learnSwift.playground/Contents.swift
@@ -0,0 +1,45 @@
+import UIKit
+
+//var str:String = "Hello, playground"
+//
+//print(str)
+//
+//var a:Float = 1.0
+//var b:Float = 2
+//
+//print(a+b)
+//print(a*b)
+//print(a/b)
+//
+//let c = 10 // c is a constant variable
+//
+//if c < 10 {
+//    print(c)
+//}
+//else if c == 10 {
+//    print(c)
+//}
+//else {
+//    print("Nothing")
+//}
+//
+//let alpaca = "🦙"
+//print(alpaca)
+
+for index in 1...5 {
+    print(index)
+}
+
+var counter = 0
+
+while counter < 10 {
+    print("ß∂ß")
+    counter = counter + 1
+}
+
+
+
+
+
+
+
---
+++ learnSwift_While_Loop.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var counter = 0;
+
+while counter < 10 {
+    print("🦙")
+    counter += 1
+}
