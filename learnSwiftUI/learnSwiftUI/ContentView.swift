eb79ea9e8dc9e0541fdd35fb80b826a7d640c3a5
廖哲澄 <liaozhedeng@liaozhedengdeMacBook-Pro.local>
2021-11-13 14:03:01 +0800

    UI project

--- learnSwiftUI/learnSwiftUI/ContentView.swift
+++ learnSwiftUI/learnSwiftUI/ContentView.swift
@@ -9,18 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack{
-            Image("logo")
-                .resizable()
-                .aspectRatio(contentMode:.fit)
-            
-            Spacer()
-            
-            Text("Hello World!")
+        ZStack {
+            Image("background").resizable().ignoresSafeArea()
+            VStack {
+                Image("logo")
+                Spacer()
+                HStack(spacing: 80) {
+                    Image("card2").cornerRadius(5)
+                    Image("card3").cornerRadius(5)
+                }
+                Spacer()
+                Image("dealbutton")
+                Spacer()
+                HStack(spacing: 160) {
+                    VStack(spacing: 15){
+                        Text("Player").foregroundColor(Color.white)
+                        Text("0").foregroundColor(Color.white).font(.system(size: 30))
+                    }
+                    VStack(spacing: 15) {
+                        Text("CPU").foregroundColor(Color.white)
+                        Text("0").foregroundColor(Color.white).font(.system(size: 30))
+                    }
+                }
+                Spacer()
+            }
         }
     }
 }
 
-
-
-
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
